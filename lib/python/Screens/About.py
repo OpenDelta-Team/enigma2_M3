a9f21ca4b5e123cc5fed0cb191937a16ea9c18a1
Open Delta Team <86120617+OpenDelta-Team@users.noreply.github.com>
2026-05-18 21:26:24 +0200

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -36,7 +36,7 @@ class About(Screen):
 		cpu = about.getCPUInfoString()
 		AboutText += _("CPU: ") + cpu + "\n"
 		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
-		AboutText += _("OE Version: (6.0)") + "\n"
+		AboutText += _("OE Version: (6.0.M5)") + "\n"
 		AboutText += _("Build date: ") + about.getBuildDateString() + "\n"
 		AboutText += _("Last update:  18/05/2026") + "\n"
 		AboutText += _("Forum: https://areadeltasat.net") + "\n"
